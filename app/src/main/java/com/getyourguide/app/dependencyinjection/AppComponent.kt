9dfd2b179a58505b939a038349ff571627baf8f8
zmwas <zacmwangi94@gmail.com>
2020-07-31 19:05:15 +0300

    minor refactor

--- app/src/main/java/com/getyourguide/app/dependencyinjection/AppComponent.kt
+++ app/src/main/java/com/getyourguide/app/dependencyinjection/AppComponent.kt
@@ -3,11 +3,19 @@ package com.getyourguide.app.dependencyinjection
 import com.getyourguide.app.application.GetYourGuideApplication
 import dagger.BindsInstance
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AndroidSupportInjectionModule::class, ActivityModule::class, DataModule::class, FragmentModule::class, ViewModelModule::class])
+@Component(
+    modules = [AndroidInjectionModule::class,
+        AndroidSupportInjectionModule::class,
+        ActivityModule::class,
+        DataModule::class,
+        FragmentModule::class,
+        ViewModelModule::class]
+)
 interface AppComponent {
 
     @Component.Builder
