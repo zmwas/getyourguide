72f41de4c8ef547a5aab24f046b6c3bc6190a97f
zmwas <zacmwangi94@gmail.com>
2020-07-31 19:03:33 +0300

    minor refactor

--- app/src/main/java/com/getyourguide/app/application/GetYourGuideApplication.kt
+++ app/src/main/java/com/getyourguide/app/application/GetYourGuideApplication.kt
@@ -2,15 +2,20 @@ package com.getyourguide.app.application
 
 import android.app.Activity
 import android.app.Application
+import androidx.fragment.app.Fragment
 import com.getyourguide.app.dependencyinjection.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-class GetYourGuideApplication : Application(), HasActivityInjector {
+class GetYourGuideApplication : Application(), HasActivityInjector, HasSupportFragmentInjector {
     @Inject
     lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+
+    @Inject
+    lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
 
     override fun onCreate() {
         super.onCreate()
@@ -19,5 +24,9 @@ class GetYourGuideApplication : Application(), HasActivityInjector {
 
     override fun activityInjector(): AndroidInjector<Activity> {
         return activityInjector
+    }
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
+        return fragmentInjector
     }
 }
