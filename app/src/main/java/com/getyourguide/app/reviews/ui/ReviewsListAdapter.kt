8b7205623a4aab5aa9308fdd827214ed341f0450
zmwas <zacmwangi94@gmail.com>
2020-08-01 20:29:31 +0300

    minor refactor

--- app/src/main/java/com/getyourguide/app/reviews/ui/ReviewsListAdapter.kt
+++ app/src/main/java/com/getyourguide/app/reviews/ui/ReviewsListAdapter.kt
@@ -51,9 +51,7 @@ class ReviewsListAdapter(private val itemClickedCallback: ItemClickedCallback) :
 
     override fun getItemViewType(position: Int): Int {
         return when {
-            networkState.status == Status.RUNNING && position == itemCount - 1 -> {
-                0
-            }
+            networkState.status == Status.RUNNING && position == itemCount - 1 -> 0
             else -> 1
         }
     }
